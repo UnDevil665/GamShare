0b7d480af969672ba4e4a978c8e2ba2449087319
Сергей Герклоц <Gerklots.S.S@yandex.ru>
2020-05-15 18:56:43 +0300

    --updated venv structure

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Gerkl\PycharmProjects\untitled\venv
+set VIRTUAL_ENV=D:\PythonApps\Luhn algorithm\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
